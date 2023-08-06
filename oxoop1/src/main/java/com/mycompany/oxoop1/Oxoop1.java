c73ec17389598bf1456d4e38fc48cf6cf07d05a5
Titapon Keawbua <64160047@go.buu.ac.th>
2023-08-07 00:03:53 +0700

    set

---
+++ oxoop1/src/main/java/com/mycompany/oxoop1/Oxoop1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oxoop1;
+
+/**
+ *
+ * @author minnie
+ */
+public class Oxoop1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
